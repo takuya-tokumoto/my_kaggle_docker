8da132f1cde38b5315d5db50f7fa9c64ddb035d1
Ubuntu <ubuntu@ip-172-31-15-192.ap-northeast-1.compute.internal>
2024-03-22 03:25:16 +0000

    refix

--- run.sh
+++ run.sh
@@ -7,6 +7,6 @@ mkdir -p $MOUNT_POINT
 # ここで AWS_S3_BUCKET と AWS_S3_MOUNT_POINT は環境変数としてdocker-compose.yml
 # またはDocker run コマンドで設定する必要があります。
 # s3fs $AWS_S3_BUCKET $MOUNT_POINT -o allow_other -o use_path_request_style
-mount-s3 $AWS_S3_BUCKE $MOUNT_POINT
+mount-s3 $AWS_S3_BUCKET $MOUNT_POINT
 
 jupyter notebook --ip=0.0.0.0 --allow-root --no-browser --NotebookApp.token="kaggle" --notebook-dir=/kaggle
